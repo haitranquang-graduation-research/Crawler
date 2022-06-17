811f69970ec50c8363ae56179855fd2db2234e83
haitranquang-official <haitranquang.official@gmail.com>
2022-06-17 13:12:25 +0000

    Crawl multiple urls

--- generic_crawler.py
+++ generic_crawler.py
@@ -6,7 +6,31 @@ from flask import Flask
 import config
 app = Flask(__name__)
 api = Api(app)
-print('Hello')
+
+
+def general_crawl(url, site):
+    rq = requests.get(url)
+    r = rq.text.encode("utf8")
+    pq = PyQuery(r)
+    for element in site['elements']:
+        if (element['type'] == 'HREF'):
+            # print(element['path'])
+            title_elements = [title for title in pq(
+                element['path']).items()]
+            # print(len(title_elements))
+            for title_element in title_elements:
+                try:
+                    url = title_element.attr('href')
+                    if url.startswith('/'):
+                        url = url[1:]
+                    if (site['name'] not in url) and (not url.startswith('https')):
+                        url = site['domain'] + url
+                    # print(url)
+                    url_dto = {}
+                    url_dto['url'] = url
+                    requests.post(config.get_parser_url(), json=url_dto)
+                except:
+                    continue
 
 
 @app.route('/crawl', methods=['GET'])
@@ -15,30 +39,11 @@ def crawl():
     sites = rq.json()
     for site in sites:
         # print(site['domain'])
-        rq = requests.get(site['domain'])
-        r = rq.text.encode("utf-8")
-        pq = PyQuery(r)
-
-        for element in site['elements']:
-            if (element['type'] == 'HREF'):
-                # print(element['path'])
-                title_elements = [title for title in pq(
-                    element['path']).items()]
-                # print(len(title_elements))
-                for title_element in title_elements:
-                    try:
-                        url = title_element.attr('href')
-                        if url.startswith('/'):
-                            url = url[1:]
-                        if (site['name'] not in url) and (not url.startswith('https')):
-                            url = site['domain'] + url
-                        # print(url)
-                        url_dto = {}
-                        url_dto['url'] = url
-                        requests.post(config.get_parser_url(), json=url_dto)
-                    except:
-                        continue
+        general_crawl(site['domain'], site)
+        for path in site['crawlPaths']:
+            general_crawl(path['path'], site)
     return 'Success'
+
 
 @app.route('/recrawl', methods=['GET'])
 def recrawl():
@@ -46,11 +51,12 @@ def recrawl():
     urls = rq.json()
     for url in urls:
         try:
-            print(url)
+            # print(url)
             requests.post(config.get_parser_url(), json=url)
         except:
             continue
     return 'Success'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5200)
